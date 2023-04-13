f729198f7ec4d3cb6d26aa4af85515a9d5ca080f
Ndibechinonso <edwardndibe@gmail.com>
2023-04-13 07:42:36 +0100

    first correction

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import React from 'react';
 function App() {
   const [query, setQuery] = useState("")
   return (
+        //  I see atleast three different components here, try separating 
     <div className='center'>
     <div className="App">
      <input text="type" placeholder='Search by name..' 
-     className='search' onChange={e=> setQuery(e.target.value)}></input>
+     value={query} // Your input should have a value attribute
+     className='search' onChange={e=> { if(query.length < 1 && e.target.value.trim().length < 1) return; //the input needs a validation that prevents the user from starting the search value with a space
+      setQuery(e.target.value)}} />
     <ul className='list'>
-      {details.filter((details)=> details.firstName.toLocaleLowerCase().includes(query)
+      {details.filter((details)=> details.firstName.toLowerCase().startsWith(query.toLowerCase()) || details.lastName.toLowerCase().startsWith(query.toLowerCase()) // remember that you are searching for a name, meaning the search value has to be the begiining of the name, startsWith is more effective than includes at this point
       ).map((details)=>(
       <li key={details.id} className='listItems'>
         <div className='image'>
